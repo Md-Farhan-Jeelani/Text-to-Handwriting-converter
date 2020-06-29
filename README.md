03eec8cc1be5ceda7a2d54918c99e6470bab8836
Md Farhan Jeelani <farhannielit2017@gmail.com>
2020-06-29 22:44:59 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,25 @@
+<p align="center">
+	<h1 align="center"> Text to Handwriting converter </h1>
+	<h4 align="center"> Convert text files into handwritten PDFs <h4>
+</p>
+<br>
+
+## Functionalities
+- [X] Convert a text document (.txt file) into a PDF file with the text content handwritten
+
+## Pre-requisites:
+- [X] python 3.5 or 3.6
+- [X] dependencies from requirements.txt
+
+## Installing required python dependencies
+- Clone this repository onto your system. 
+- Then, create a virtual environment and install the packages from requirements.txt.
+
+## Directions to run
+- Run the following command in the project directory:
+```
+python txttohandwriting.py
+```
+
+## Contributors:
+- [Md Farhan Jeelani](https://github.com/Md-Farhan-Jeelani)
---
+++ txttohandwriting.py
@@ -0,0 +1,85 @@
+from PIL import Image
+BG = Image.open("myfont/bg.png")
+sizeOfSheet =BG.width
+gap, _  = 0,0
+allowedChars = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM,.-?!() 1234567890'
+def writee(char):
+    global gap, _
+    if char == '\n':
+        pass
+    else:
+        char.lower()
+        cases = Image.open("myfont/%s.png"%char)
+        BG.paste(cases, (gap, _))
+        size = cases.width
+        gap += size
+        del cases
+
+def letterwrite(word):
+    global gap, _
+    if gap > sizeOfSheet - 95*(len(word)):
+        gap = 0
+        _ += 200
+    for letter in word:        
+        if letter in allowedChars:
+            if letter.islower():
+                pass
+            elif letter.isupper():
+                letter = letter.lower()
+                letter += 'upper'            
+            elif letter == '.':
+                letter = "fullstop"
+            elif letter == '!':
+                letter = 'exclamation'
+            elif letter == '?':
+                letter = 'question'
+            elif letter == ',':
+                letter = 'comma'
+            elif letter == '(':
+                letter = 'braketop'
+            elif letter == ')':
+                letter = 'braketcl'
+            elif letter == '-':
+                letter = 'hiphen'
+            writee(letter)
+def worddd(Input):
+    wordlist=Input.split(' ')
+    for i in wordlist:
+        letterwrite(i)
+        writee('space')
+if __name__ == '__main__':
+    try:
+        with open('boom.txt', 'r') as file:
+            data = file.read().replace('\n', '')
+        l=len(data)
+        nn=len(data)//600
+        chunks, chunk_size = len(data), len(data)//(nn+1)
+        p=[ data[i:i+chunk_size] for i in range(0, chunks, chunk_size) ]
+        
+        for i in range(0,len(p)):
+            worddd(p[i])
+            writee('\n')
+            BG.save('%doutt.png'%i)
+            BG1= Image.open("myfont/bg.png")
+            BG=BG1
+            gap = 0
+            _ =0
+    except ValueError as E:
+        print("{}\nTry again".format(E))
+
+from fpdf import FPDF
+from PIL import Image
+
+imagelist=[]
+for i in range(0,len(p)):
+    imagelist.append('%doutt.png'%i)
+
+#Converting images to pdf
+#Source:https://datatofish.com/images-to-pdf-python/
+
+#Now I am opening each images 
+for PNG_FILE in imagelist:
+    rgba = Image.open(PNG_FILE)
+    rgb = Image.new('RGB', rgba.size, (255, 255, 255))  # white background
+    rgb.paste(rgba, mask=rgba.split()[3])               # paste using alpha channel as mask
+    rgb.save('final_output.pdf', append=True)  #Now save multiple images in same pdf file
